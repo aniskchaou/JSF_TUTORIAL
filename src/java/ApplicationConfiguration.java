eba2c676cbdedfc35a6fe56f0656078c65f2f283
anis <anis@example.com>
2019-02-24 01:08:33 +0100

    first commit

---
+++ src/java/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig( version = FacesConfig.Version.JSF_2_3 )         // Activation de CDI
+public class ApplicationConfiguration {
+    
+}
